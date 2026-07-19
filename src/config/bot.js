c751cad00204a54dd311fa2dfdba7a775ba993a7
yfass2009-maker <yfass2009@gmail.com>
2026-07-19 12:12:52 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "MTA", // required by Discord API, not shown in the client
         state: "صيانه",     // this is what people actually see
         type: 5,               // Custom
       },
